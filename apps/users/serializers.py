1f2b1aa72c7961ff35f19f9fe841289c62663d1d
DaTrEvTeR <rozhkovm176@gmail.com>
2025-03-26 15:20:07 +0200

    Release password reset with email

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 User = get_user_model()
 
@@ -12,4 +15,40 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+        return user
+
+
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    new_password = serializers.CharField(write_only=True, min_length=8)
+
+    def validate(self, data):
+        """
+        Проверка существования пользователя и валидации токена.
+        """
+        uidb64 = self.context.get("uidb64")
+        token = self.context.get("token")
+
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = User.objects.get(pk=uid)
+        except (User.DoesNotExist, ValueError, TypeError):
+            raise ValidationError({"uidb64": "Неверный идентификатор пользователя."})
+
+        if not default_token_generator.check_token(user, token):
+            raise ValidationError({"token": "Недействительный токен."})
+
+        self.context["user"] = user  # Сохраняем пользователя для использования в `save()`
+        return data
+
+    def save(self):
+        """
+        Устанавливает новый пароль пользователю.
+        """
+        user = self.context["user"]
+        user.set_password(self.validated_data["new_password"])
+        user.save()
         return user
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,4 +5,6 @@ app_name = "apps.users"
 
 urlpatterns: list[path] = [
     path("register/", views.UserRegisterView.as_view(), name="register"),
+    path("password_reset/", views.PasswordResetAPIView.as_view(), name="password_reset"),
+    path("reset/<uidb64>/<token>/", views.PasswordResetConfirmAPIView.as_view(), name="password_reset_confirm"),
 ]
